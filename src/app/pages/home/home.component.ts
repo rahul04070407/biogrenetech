de93cbf19a8defe839b4886e148e059fdf8d1f14
rahul04070407 <rahulp@vareli.co.in>
2025-05-23 16:09:26 +0530

    about us route

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit , OnDestroy{
+ @ViewChild('aboutSection') aboutSection!: ElementRef;
+  private routerSub!: Subscription;
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngAfterViewInit() {
+    this.routerSub = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        // Wait a tick so view initializes
+        setTimeout(() => {
+          const fragment = this.route.snapshot.fragment;
+          if (fragment === 'about' && this.aboutSection) {
+            this.aboutSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+          }
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.routerSub?.unsubscribe();
+  }
 }
